7f959e4f69434293d2d51881d0aa96a7533e809e
David Arnold <david.arnold@qbe.com>
2024-07-31 13:22:17 +1000

    Remove padding from codesnaps by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -365,6 +365,7 @@ cmp.setup {
 
 require("codesnap").setup({
   watermark = "",
+  bg_padding = 0,
 })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

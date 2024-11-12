a0021a62512b61a12e3e872b09f83ed506b7dc42
DavidJArnold <david.arnold@qbe.com>
2024-11-13 10:48:18 +1100

    Add gpull/gpush functions

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -1,4 +1,8 @@
 alias gs="git status"
 alias gd="git diff"
 
+alias gpull="git branch --show-current | git pull origin"
+alias gpush="git branch --show-current | git push origin"
+
+
 alias venv="source venv/bin/activate"

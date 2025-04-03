097a7eeaf9c61f9e11fcae8f866c1a1f361eaccc
DavidJArnold <david.arnold@qbe.com>
2025-04-03 14:17:01 +1100

    (neovim) Remove some unused language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -160,7 +160,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vimdoc' },
+  ensure_installed = { 'lua', 'python', 'rust' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
@@ -280,7 +280,7 @@ local servers = {
   -- gopls = {},
   pyright = {},
   -- rust_analyzer = {},
-  ts_ls = {},
+  -- ts_ls = {},
 
   lua_ls = {
     Lua = {
